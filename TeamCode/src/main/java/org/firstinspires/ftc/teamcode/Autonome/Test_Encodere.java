fec4c87ef47d4b53b9036fd82fbb99ad009d3de9
DVD-R-2 <91427703+DVD-R-2@users.noreply.github.com>
2022-02-12 15:28:08 +0200

    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonome/Test_Encodere.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonome/Test_Encodere.java
@@ -11,8 +11,9 @@ import org.firstinspires.ftc.teamcode.Hardware.HardwareM;
 @Autonomous(name = "Test Encodere")
 public class Test_Encodere extends LinearOpMode
 {
-    DcMotor roataStanga = null, roataDreapta = null;
-    private ElapsedTime runtime = new ElapsedTime();
+    DcMotor roataStanga = null,
+            roataDreapta = null;
+
     @Override
     public void runOpMode() throws InterruptedException {
         roataStanga = hardwareMap.get(DcMotor.class, "motorStanga");
@@ -22,15 +23,7 @@ public class Test_Encodere extends LinearOpMode
 
         // Wait for start while displaying encoder values  (Note: these may not start at zero)
         // This i   s just for testing
-        while(!isStarted()) {
-            telemetry.addData("LeftPosition", roataStanga.getCurrentPosition());
-            telemetry.addData("RightPosition", roataDreapta.getCurrentPosition());
-            telemetry.update();
-        }
-
-        // We are now running opmode
-        // Start clock at zero
-        runtime.reset();
+        waitForStart();
 
         // Reset encoders
         roataDreapta.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -44,11 +37,11 @@ public class Test_Encodere extends LinearOpMode
         roataDreapta.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         roataStanga.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        roataStanga.setPower(1.0);
-        roataDreapta.setPower(1.0);
+        roataStanga.setPower(.3);
+        roataDreapta.setPower(.3);
 
         // Loop while we approach the target.  Display position as we go
-        while(roataDreapta.isBusy() && roataStanga.isBusy() && opModeIsActive()) {
+        while(opModeIsActive() && (roataDreapta.isBusy() || roataStanga.isBusy())) {
             telemetry.addData("Left Position / Target :", "%7d / %7d",
                               roataStanga.getCurrentPosition(), roataStanga.getTargetPosition());
             telemetry.addData("Right Position / Target :", "%7d / %7d",
@@ -59,6 +52,9 @@ public class Test_Encodere extends LinearOpMode
         // We are done, turn motors off and switch back to normal driving mode.
         roataStanga.setPower(0);
         roataDreapta.setPower(0);
+
+        roataStanga.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        roataDreapta.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
     }
 
